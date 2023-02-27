8df25627e0461bb050999d771e0331d178e0814d
Aakashi <Aakashi@AAKASHLAP2016>
2023-02-28 01:31:09 +0530

    Add project files.

---
+++ Controllers/SchoolAdmissionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentAdmissioionManagement.Models;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudentAdmissioionManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolAdmissionController : ControllerBase
+    {
+        // GET: api/<SchoolAdmissionController>
+        [HttpGet]
+        public IEnumerable<StudentsAdmissionModels> Get()
+        {
+            StudentsAdmissionModels admissionobj1 = new StudentsAdmissionModels();
+            StudentsAdmissionModels admissionobj2 = new StudentsAdmissionModels();
+            admissionobj1.StudentID = 1;
+            admissionobj1.StudentName = "Adam";
+            admissionobj1.StudentClass = "X";
+            admissionobj1.DateofJoining = DateTime.Now;
+            admissionobj2.StudentID = 2;
+            admissionobj2.StudentName = "Brad";
+            admissionobj2.StudentClass = "IX";
+            admissionobj2.DateofJoining = DateTime.Now;
+            List<StudentsAdmissionModels> listofobj = new List<StudentsAdmissionModels>
+{
+admissionobj1,
+admissionobj2
+};
+            return listofobj;
+        }
+    }
+}
+
+
+
+
---
+++ Models/StudentsAdmissionModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudentAdmissioionManagement.Models
+{
+    public class StudentsAdmissionModels
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string StudentClass { get; set; }
+        public DateTime DateofJoining { get; set; }
+
+    }
+}
